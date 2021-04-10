13cfcdf09acfb748bdb841e436bd4c33851f25d9
Nicholas <Lyit2017@163.com>
2021-04-10 12:29:26 +0800

    LinkedList-机号1-20200410

---
+++ src/main/java/LinkedList/Main.java
@@ -0,0 +1,30 @@
+package LinkedList;
+/**
+ * @author qiwl
+ * @version 1.0
+ * @description
+ * @date 2021/4/10 11:40
+ */
+ 
+public class Main {
+    public static void main(String[] args) {
+
+        LinkedList<Integer> linkedList = new LinkedList<Integer>();
+        for(int i = 0 ; i < 5 ; i ++){
+            linkedList.addFirst(i);
+            System.out.println(linkedList);
+        }
+
+        linkedList.add(2, 666);
+        System.out.println(linkedList);
+
+        linkedList.remove(2);
+        System.out.println(linkedList);
+
+        linkedList.removeFirst();
+        System.out.println(linkedList);
+
+        linkedList.removeLast();
+        System.out.println(linkedList);
+    }
+}
