568f664ad4b43787f98051045cb4b91c10c44579
Nicholas <Lyit2017@163.com>
2021-07-13 21:28:20 +0800

    AVL树

---
+++ src/main/java/avlTree/AVLSet.java
@@ -0,0 +1,45 @@
+package avlTree;
+
+import set.Set;
+
+/**
+ * @author qiwl
+ * @version 1.0
+ * @description
+ * @date 2021/7/13 21:15
+ */
+ 
+public class AVLSet<E extends Comparable<E>> implements Set<E> {
+
+    private AVLTree<E,Object> avl;
+
+    public AVLSet(){
+        avl = new AVLTree<>();
+    }
+
+    @Override
+    public int getSize(){
+        return avl.getSize();
+    }
+
+    @Override
+    public boolean isEmpty(){
+        return avl.isEmpty();
+    }
+
+    @Override
+    public void add(E e){
+        avl.add(e, null);
+    }
+
+    @Override
+    public boolean contains(E e){
+        return avl.contains(e);
+    }
+
+    @Override
+    public void remove(E e){
+        avl.remove(e);
+    }
+
+}
