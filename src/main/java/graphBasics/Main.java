5dd83fbeabdd1fdbf0c665fe80fc062b48a382a5
Nicholas <Lyit2017@163.com>
2022-04-01 20:46:58 +0800

    索引堆

---
+++ src/main/java/graphBasics/Main.java
@@ -0,0 +1,16 @@
+package graphBasics;
+
+import static sun.util.calendar.CalendarUtils.mod;
+
+/**
+ * @author qiwl
+ * @version 1.0
+ * @description
+ * @date 2022/3/30 19:56
+ */
+ 
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(mod(7,5));
+    }
+}
